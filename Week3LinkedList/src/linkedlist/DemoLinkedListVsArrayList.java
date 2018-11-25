0a970c14d61e37803754fff0b6acc308d953b731
hanbioinformatica <martijn.vanderbruggen@han.nl>
2018-11-25 12:15:14 +0100

    queue update

--- Week3LinkedList/src/linkedlist/DemoLinkedListVsArrayList.java
+++ Week3LinkedList/src/linkedlist/DemoLinkedListVsArrayList.java
@@ -18,28 +18,32 @@ class DemoLinkedListVsArrayList {
 
     public static void addElements(List l, int a) {
         rand = new Random(1000);
-        int aantal = 1000;
-
-        for (int i = 0; i < aantal; i++) {
+        for (int i = 0; i < a; i++) {
             lijst.add(0, rand.nextInt(100));
         }
-    }
+      }
 
-    public static void main(String[] args) {
+    
+    private static void timeAdding(){
+        int aantalKeer = 10^10;
         lijst = new ArrayList<Integer>();
         long startTime = System.nanoTime();
-        addElements(lijst, 1000000000);
+        addElements(lijst, aantalKeer);
         long endTime = System.nanoTime();
         long tijd1 = endTime - startTime;
         System.out.println("Tijd ArrayList: " + (double) tijd1 / 1000000000 + " seconden");
         lijst = new LinkedList<Integer>();
         startTime = System.nanoTime();
-        addElements(lijst, 1000000000);
+        addElements(lijst, aantalKeer);
         endTime = System.nanoTime();
         long tijd2 = endTime - startTime;
         System.out.println("Tijd LinkedList: " + (double) tijd2 / 1000000000 + " seconden");
-        System.out.println((float)tijd1/(float)tijd2);
-        
-
+        System.out.println("LinkedList is "+(float)tijd1/(float)tijd2+" keer sneller");
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        timeAdding();  
     }
 }
