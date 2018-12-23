abc4288d38824d4575ad6e3d379d190ed801c745
martijn <Martijn.vanderBruggen@han.nl>
2018-12-23 16:56:21 +0100

    Thread demo gemaakt en toegevoegd

---
+++ Week7Threads/src/threadGUI/Lijn.java
@@ -0,0 +1,57 @@
+package threadGUI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+
+/**
+ * @author Martijn van der Bruggen
+ * @version Dec 22, 2018 (c) HAN University Instituut Toegepaste Biologie en
+ * Chemie Bio-informatica
+ *
+ */
+public class Lijn extends Thread {
+
+    int lijnNummer = 0;
+    static int x1 = 10;
+    int y1 = 0;
+    static int lengte = 100;
+    Graphics g;
+    JLabel info;
+
+    Color kleur = Color.blue;
+
+    public Lijn(int nummer, Graphics gg, JLabel jl) {
+        g = gg;
+        info = jl;
+        lijnNummer = nummer; //threadnummer
+        y1 = 20 + lijnNummer * 20; // Positie op y-as
+    }
+
+    private void sleep(int msec) {
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void drawLine() {
+        g.setColor(kleur);
+        info.setText("Thread: " + lijnNummer);
+        for (int i = 0; i < lengte; i++) {
+            x1 += 1;
+            g.fillRect(x1, y1, 1, 5);
+            sleep(10);
+        }
+    }
+    
+
+    public void run(){
+        drawLine();
+        System.out.println("Doe het!");
+    }
+
+}
