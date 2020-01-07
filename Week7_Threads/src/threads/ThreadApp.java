2335f019430914b1e545313b70b6b65ab369b7bf
hanbioinformatica <martijn.van.der.bruggen@gmail.com>
2020-01-07 23:07:50 +0100

    threads

--- Week7_Threads/src/threads/ThreadApp.java
+++ Week7_Threads/src/threads/ThreadApp.java
@@ -3,7 +3,7 @@ package threads;
 public class ThreadApp {
 
     public static void main(String args[]) {
-        new SimpleThread("bio").start();
-        new SimpleThread("informatica").start();
+        new SimpleThread("bio").run();
+        new SimpleThread("informatica").run();
     }
 }
